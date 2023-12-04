1e5bdab4160867ace6af722b89ac25d55912439a
Mentenansu <tumrong.sae@dome.tu.ac.th>
2023-12-04 21:40:54 +0700

    Update JUNITSTACK.java

--- src/core/JUNITSTACK.java
+++ src/core/JUNITSTACK.java
@@ -1,40 +1,61 @@
 package core;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
 import junit.framework.TestCase;
+
+//https://github.com/Kittithara6509611544/TDD6509611544.git
 
 public class JUNITStack extends TestCase {
 	
+
+	@Test
 	public void testCreateNewEmptyStack() {
-		
-		Stack s1= new Stack();
+		Stack s1 = new Stack(20);
 		int size = s1.getSize();
 		
 		assertEquals(0, size);
 		assertTrue(s1.isEmpty());
-				
 	}
 	
-	public void testPushElmToTop() {
-		
-		Stack s1 = new Stack();
-		assertFalse(s1.isFull());
+	@Test
+	public void testPushStack() throws Exception {
+		Stack s1 = new Stack(20);
 		s1.push(1);
-	
-		assertEquals(1, s1.top());
-		
+		int top = s1.getTop();
+		assertEquals(1, top);
 	}
 	
-	public void testLastInFirstOut() {
+	@Test
+	public void testLastInFirstOut() throws Exception {
+		Stack s1 = new Stack(20);
+		s1.push(1);
+		int pop = s1.pop();
+		assertEquals(1, pop);
 		
-		Stack s1 = new Stack();
-		s1.push(new Integer(1));
-		Object top = s1.top();
-		int size = s1.getSize();
+		for(int i=0;i<10;i++) {
+			s1.push(i);
+		}
 		
-		assertEquals(1, s1.pop());
-		assertEquals(size-1, s1.getSize());
+		for(int i=9;i<0;i--) {
+			pop = s1.pop();
+			assertEquals(i, pop);
+		}
 	}
 	
-	
+	@Test
+	public void testPushLimitedSizeStack() throws Exception {
+		Stack s1 = new Stack(20);
+		for(int i=0;i<20;i++) {
+			s1.push(i);
+		}
+		assertThrows(Exception.class, 
+				()->{
+					s1.push(1);
+				}
+				);
+	}
+
 }
-  
